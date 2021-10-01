8603d8af17ee9424bf9a9ac3d18f6e3b2d572233
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-01 10:48:57 +0530

    Finalize a crazy animation

--- math-usage/ts/fun-with-math.ts
+++ math-usage/ts/fun-with-math.ts
@@ -5,14 +5,22 @@ class Moveable{
 
     private elm: JQuery;
 
-    constructor(private dy: number = -5, private dx: number = 10){
+    constructor(private dy: number = 0, private dx: number = 0){
         this.elm = $('<div class="moveable"></div>');
+        this.dy = 4 + ((Math.random() * 15) * Math.sin((Math.random() * 361) * Math.PI / 180)); // +-[4 - 15]
+        this.dx = 4 + ((Math.random() * 15) * Math.sin((Math.random() * 361) * Math.PI / 180)); // +-[4 - 15]
 
+        // 180deg = PIrad
+        // 1deg = PIrad / 180
+
+        this.elm.css('background-color', `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`);
+        this.elm.css('border-radius', `${Math.floor(Math.random() * 101)}%`); // 100.2, 100.8 => 100
+        this.elm.css('transform', `rotate(${Math.random() * 361}deg) scale(${Math.random()})`)
         $("body").append(this.elm);
 
         this.elm.offset({
-            top: Math.random() * $(window).height()!,
-            left: Math.random() * $(window).width()!
+            top: Math.random() * ($(window).height()! - this.elm.height()!),
+            left: Math.random() * ($(window).width()! - this.elm.width()!)
         });
     }
 
@@ -41,6 +49,19 @@ class Moveable{
 
 }
 
-const mo = new Moveable();
+const moveables: Moveable[] = [];
 
-setInterval(()=> mo.move(), 10);
+for (let i = 0; i < 50; i++) {
+    moveables.push(new Moveable());
+}
+
+
+setInterval(()=> moveables.forEach(mo => mo.move()), 50);
+
+// setInterval(()=> {
+
+//     moveables.forEach(mo => {
+//         mo.move();
+//     })
+
+// }, 10);
